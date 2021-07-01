e6eafda18973a3e90a18f227b7921bc7298f1c21
GHuss7 <guntherhusselmann@gmail.com>
2021-07-01 15:07:27 +0200

    Commented matrix prints

--- EvaluateRouteSet.py
+++ EvaluateRouteSet.py
@@ -534,8 +534,8 @@ def fullPassengerEvaluation(routes, travelTimes,DemandMat, total_demand,n,tp,wt)
     D, changes = FastFloydChanges(routeadj,changes)
     SPMatrix,ChMatrix = shortest_paths_matrixChanges(D, inv_map, t, n, changes)
     #print(ChMatrix)
-    a = np.array(ChMatrix)
-    np.savetxt('ChangesMat.txt', a.astype(int), fmt='%i', delimiter=' ')   # X is an array
+    #a = np.array(ChMatrix)
+    #np.savetxt('ChangesMat.txt', a.astype(int), fmt='%i', delimiter=' ')   # X is an array
     d0, d1, d2, drest, ATT, total_ATT, total_TT = EvaluateChanges(n, SPMatrix, ChMatrix, DemandMat, total_demand, tp, wt)
     noRoutes = len(routes)
     return(ATT,d0,d1,d2,drest,noRoutes,longest,shortest)
